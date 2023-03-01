774c9110a2c5139ce5059a4bc4796156cb4686fc
mohamed-ibrahim <engmohamed2017@hotmail.com>
2023-03-01 17:42:28 +0200

    fix axios error

--- server/modules/users/users.module.ts
+++ server/modules/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule } from '@server/core/http/http.module';
 
 @Module({
   imports: [HttpModule],
